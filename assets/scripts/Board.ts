b9f84904e7a8c38cdb1b8cb68cc3b930ef3bce48
changl pan <sdpcpcl@hotmail.com>
2023-11-26 11:24:49 +0800

    修改了棋盘布局和添加小动物头像来区分各个棋子

--- assets/scripts/Board.ts
+++ assets/scripts/Board.ts
@@ -1,33 +1,33 @@
 import { _decorator, CCInteger, Component, instantiate, Node, Prefab, v2, Vec2 } from 'cc';
 const { ccclass, property } = _decorator;
 
-@ccclass('Cells')
-export class Cells extends Component {
+@ccclass('Board')
+export class Board extends Component {
     @property({ type: Prefab, displayName: '棋盘格子预制体' })
     cell_prefab: Prefab = null;
     @property({ type: CCInteger, displayName: '格子间距' })
     cell_spacing: number = 1;
 
-    private _cell_size: Vec2 = v2(64, 64)
+    private _cell_size: Vec2 = v2(100, 100)
 
 
     start() {
-        this.initCells();
+        this.initBoard();
     }
     /**
-     * 初始化棋盘
+     * # 初始化棋盘  
+     * - 通过循环来生成一个6*9的棋盘
      */
-    initCells() {
+    initBoard() {
         if (this.cell_prefab == null) return;
-        for (let i = 0; i < 10; i++) {
-            for (let j = 0; j < 15; j++) {
+        for (let i = 0; i < 6; i++) {
+            for (let j = 0; j < 9; j++) {
                 let temp_cell = instantiate(this.cell_prefab);
                 this.node.addChild(temp_cell);
                 temp_cell.setPosition(
                     (i+1) * (this._cell_size.x + this.cell_spacing),
                     (j+1) * (this._cell_size.y + this.cell_spacing)
                 );
-                console.log(`坐标: X->${(i+1) * (this._cell_size.x + this.cell_spacing)} Y-> ${(j+1) * (this._cell_size.y + this.cell_spacing)}`)
             }
         }
     }
